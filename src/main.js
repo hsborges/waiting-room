5f392c413f9ee3c086347a38da696987284291b2
Hudson Silva Borges <hudsonsilbor@gmail.com>
2020-08-10 16:40:16 -0400

    feat: add analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import VueGtag from "vue-gtag";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_ANALYTICS }
+});
 
 new Vue({
   router,
